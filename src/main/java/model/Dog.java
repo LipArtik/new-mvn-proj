8808ac3eac84904462a2e1e8e6405a3d80b86740
Artsiom Lipnitski <lipart777@gmail.com>
2023-07-05 21:00:08 +0300

    oooooooooo

--- src/main/java/model/Dog.java
+++ src/main/java/model/Dog.java
@@ -1,2 +1,30 @@
-package model;public class Doc {
+package model;
+
+public class Dog {
+
+
+    //
+    private String dog;
+    private int id ;
+
+
+    public String getDog() {
+        return dog;
+    }
+
+    public void setDog(String dog) {
+        this.dog = dog;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Dog is good friend");
+    }
 }
